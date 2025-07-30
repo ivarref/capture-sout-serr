a1e18db1169a609861f14aaadce86e40cf35155a
ire <ivar.refsdal@sikt.no>
2025-07-30 16:48:04 +0200

    Ignore SIGINT in prefix.py

--- nrepl-consumer/prefix.py
+++ nrepl-consumer/prefix.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
 import sys
+import signal
 
 if __name__ == "__main__":
     assert 2 == len(sys.argv)
+
+    def signal_handler(sig, frame):
+        pass
+    signal.signal(signal.SIGINT, signal_handler)
+
     with open('./.max_prefix.txt') as fd:
         max_prefix = fd.read().strip()
     prefix = sys.argv[1]
