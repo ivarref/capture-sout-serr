24973c67787383e86eb20b594a2910b69afaa539
ire <ivar.refsdal@sikt.no>
2025-07-30 14:44:39 +0200

    WIP

--- nrepl-consumer/src/com/github/ivarref/run_server.clj
+++ nrepl-consumer/src/com/github/ivarref/run_server.clj
@@ -5,6 +5,8 @@
   (:import (com.github.ivarref.capturesoutserr ReplayConsumePrintStream)
            (java.io OutputStreamWriter)))
 
+(defonce debug-write-lock (Object.))
+
 (defonce original-stdout System/out)
 
 (defonce replay-stream (ReplayConsumePrintStream.))
@@ -12,8 +14,8 @@
 (defn run-server [_]
   (try
     (println "Starting nREPL server ...")
-    #_(System/setOut replay-stream)
-    #_(alter-var-root #'*out* (fn [_] (OutputStreamWriter. replay-stream)))
+    (System/setOut replay-stream)
+    (alter-var-root #'*out* (fn [_] (OutputStreamWriter. replay-stream)))
     (println "not shown on -X:run-server, but will be buffered")
     (nrepl-server/start-server :port 7888)
     @(promise)
@@ -24,28 +26,29 @@
       (println "nREPL server exiting"))))
 
 (defn debug [msg]
-  (spit "debug.log" (str msg "\n") :append true))
+  (locking debug-write-lock
+    (spit "debug.log" (str msg "\n") :append true)))
 
 (defn hook-sout []
   (debug (pr-str *out*))
   :hook-sout-ok)
 
-(defn run-client [_]
-  (try
-    (println "Starting nREPL client ...")
-    #_(System/setOut replay-stream)
-    #_(alter-var-root #'*out* (fn [_] (OutputStreamWriter. replay-stream)))
-    #_(println "not shown on -X:run-server")
-    #_(nrepl-server/start-server :port 7888)
-    (with-open [conn (nrepl/connect :host "127.0.0.1" :port 7888)]
-      (-> (nrepl/client conn 3000)    ; message receive timeout required
-          (nrepl/message {:op "eval" :code "(com.github.ivarref.run-server/hook-sout)"})
-          nrepl/response-values
-          (println)))
-    (println "Done")
-    (catch Exception e
-      (binding [*out* *err*]
-        (println "Client received exception:" e)))
-    (finally
-      (println "nREPL client exiting"))))
+#_(defn run-client [_]
+    (try
+      (println "Starting nREPL client ...")
+      #_(System/setOut replay-stream)
+      #_(alter-var-root #'*out* (fn [_] (OutputStreamWriter. replay-stream)))
+      #_(println "not shown on -X:run-server")
+      #_(nrepl-server/start-server :port 7888)
+      (with-open [conn (nrepl/connect :host "127.0.0.1" :port 7888)]
+        (-> (nrepl/client conn 3000)    ; message receive timeout required
+            (nrepl/message {:op "eval" :code "(com.github.ivarref.run-server/hook-sout)"})
+            nrepl/response-values
+            (println)))
+      (println "Done")
+      (catch Exception e
+        (binding [*out* *err*]
+          (println "Client received exception:" e)))
+      (finally
+        (println "nREPL client exiting"))))
 
